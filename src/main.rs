922d16d1b8dd95596df3dc40a5c648a0a26c1d45
detoo <daily.detoo@gmail.com>
2024-12-30 21:00:21 +0800

    wip: feat: Model complete booking parameters

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use reqwest::blocking::Client;
 use std::{fs::File, io::{self, Write}};
 use std::path::Path;
+use chrono::{DateTime, NaiveDateTime};
 use opener;
 use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE, HOST, USER_AGENT};
 use reqwest::redirect::Policy;
@@ -25,6 +26,29 @@ enum Station {
     Zuouing,
 }
 
+#[derive(Debug, PartialEq, Serialize_repr, Deserialize_repr)]
+#[repr(u8)]
+enum Trip {
+    OneWay = 0,
+    RoundTrip,
+}
+
+#[derive(Debug, PartialEq, Serialize_repr, Deserialize_repr, Default)]
+#[repr(u8)]
+enum CabinClass {
+    #[default]
+    Standard = 0,
+    Business,
+}
+
+#[derive(Debug, PartialEq, Serialize_repr, Deserialize_repr)]
+#[repr(u8)]
+enum SeatPref {
+    NoPref = 0,
+    Window,
+    Aisle,
+}
+
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Booking {
     #[serde(rename = "selectStartStation")]
@@ -33,6 +57,68 @@ struct Booking {
     dest_station: Station,
     #[serde(rename = "bookingMethod")]
     search_by: String,
+    #[serde(rename = "tripCon:typesoftrip")]
+    types_of_trip: Trip,
+
+    // TODO Implement more sophisticated logic to serialize datetime as a chrono NaiveDateTime instance
+    // outbound_datetime: NaiveDateTime,
+    #[serde(rename = "toTimeInputField")]
+    outbound_date: String,
+    #[serde(rename = "toTimeTable")]
+    outbound_time: String,
+
+    #[serde(rename = "homeCaptcha:securityCode")]
+    security_code: String,
+    #[serde(rename = "seatCon:seatRadioGroup")]
+    seat_prefer: SeatPref,
+    #[serde(default, rename = "BookingS1Form:hf:0")]
+    form_mark: String,
+    #[serde(default, rename = "trainCon:trainRadioGroup")]
+    class_type: CabinClass,
+
+    // TODO Implement more sophisticated logic to serialize datetime as a chrono NaiveDateTime instance
+    // inbound_datetime: NaiveDateTime,
+    #[serde(default, rename = "backTimeInputField")]
+    inbound_date: Option<String>,
+    #[serde(default, rename = "backTimeTable")]
+    inbound_time: Option<String>,
+
+    #[serde(default, rename = "toTrainIDInputField")]
+    to_train_id: Option<i16>,
+    #[serde(default, rename = "backTrainIDInputField")]
+    back_train_id: Option<i16>,
+
+    // TODO There must be a better way to represent this
+    #[serde(default = "default_adult_ticket", rename = "ticketPanel:rows:0:ticketAmount")]
+    adult_ticket_num: String,
+    #[serde(default = "default_child_ticket", rename = "ticketPanel:rows:1:ticketAmount")]
+    child_ticket_num: String,
+    #[serde(default = "default_disabled_ticket", rename = "ticketPanel:rows:2:ticketAmount")]
+    disabled_ticket_num: String,
+    #[serde(default = "default_elder_ticket", rename = "ticketPanel:rows:3:ticketAmount")]
+    elder_ticket_num: String,
+    #[serde(default = "default_college_ticket", rename = "ticketPanel:rows:4:ticketAmount")]
+    college_ticket_num: String,
+}
+
+fn default_adult_ticket() -> String {
+    "1F".to_string()
+}
+
+fn default_child_ticket() -> String {
+    "0H".to_string()
+}
+
+fn default_disabled_ticket() -> String {
+    "0W".to_string()
+}
+
+fn default_elder_ticket() -> String {
+    "0E".to_string()
+}
+
+fn default_college_ticket() -> String {
+    "0P".to_string()
 }
 
 fn download_captcha(client: &Client, url: &str, headers: HeaderMap, output_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
@@ -85,13 +171,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO Get booking parameters either from presets or user input
     // TODO Fake the booking parameters for now
     // TODO Use serde to model the booking parameters
+
     let booking = Booking{
         start_station: Station::Nangang,
         dest_station: Station::Zuouing,
         search_by: String::from("radio31"),
+        types_of_trip: Trip::OneWay,
+        // TODO test
+        // outbound_datetime: DateTime::parse_from_str("2025/01/21 10:00 AM", "%Y/%m/%d %H:%M")
+        outbound_date: String::from("2025/01/21"),
+        outbound_time: String::from("1000A"),
+        security_code: String::from("abcd"),
+        seat_prefer: SeatPref::Window,
+        form_mark: String::from(""),
+        class_type: CabinClass::Business,
+        inbound_date: None,
+        inbound_time: None,
+        to_train_id: None,
+        back_train_id: None,
+        adult_ticket_num: String::from("1F"),
+        child_ticket_num: String::from("0H"),
+        disabled_ticket_num: String::from("0W"),
+        elder_ticket_num: String::from("2E"),
+        college_ticket_num: String::from("0P"),
     };
     println!("booking: {:?}", booking);
     println!("booking (json): {}", serde_json::to_string(&booking).unwrap());
+
+    // TODO test
+    // let dt = NaiveDateTime::parse_from_str("2025/01/27 22:00", "%Y/%m/%d %H:%M").unwrap();
+    // println!("datetime: {}", dt);
 
     Ok(())
 }
