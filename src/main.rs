80e7a8f048fb760dbf4270e4166c9e8012071d23
detoo <daily.detoo@gmail.com>
2024-12-30 23:03:18 +0800

    wip: feat: Get session ID from cookies

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use reqwest::blocking::Client;
 use std::{fs::File, io::{self, Write}};
 use std::path::Path;
-use chrono::{DateTime, NaiveDateTime};
+use chrono::NaiveDateTime;
 use opener;
 use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE, HOST, USER_AGENT};
 use reqwest::redirect::Policy;
@@ -140,27 +140,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let booking_page_url = "https://irs.thsrc.com.tw/IMINT/?locale=tw";
     let captcha_local_path = Path::new("tmp/captcha.png");
 
-    // TODO test
-    // // Show the CAPTCHA image
-    // let mut headers = HeaderMap::new();
-    // headers.insert(HOST, HeaderValue::from_static("irs.thsrc.com.tw"));
-    // headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"));
-    // headers.insert(ACCEPT, HeaderValue::from_static("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"));
-    // headers.insert(ACCEPT_LANGUAGE, HeaderValue::from_static("zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3"));
-    // headers.insert(ACCEPT_ENCODING, HeaderValue::from_static("gzip, deflate, br"));
-    // let response = client
-    //     .get(booking_page_url)
-    //     .headers(headers.clone())
-    //     .send()?.text()?;
-    // let document = Html::parse_document(&response);
-    // let selector = Selector::parse("#BookingS1Form_homeCaptcha_passCode").unwrap();
-    // let element = document.select(&selector).next().expect("Couldn't find the captcha element");
-    // let src = element.value().attr("src").expect("Couldn't find the captcha source url");
-    // let captcha_url = ["https://irs.thsrc.com.tw", src].concat();
-    // println!("Captcha url: {}", captcha_url);
-    // download_captcha(&client, captcha_url.as_str(), headers, captcha_local_path)?;
-    // opener::open(captcha_local_path)?;
-    //
+    // Show the CAPTCHA image
+    let mut headers = HeaderMap::new();
+    headers.insert(HOST, HeaderValue::from_static("irs.thsrc.com.tw"));
+    headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"));
+    headers.insert(ACCEPT, HeaderValue::from_static("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"));
+    headers.insert(ACCEPT_LANGUAGE, HeaderValue::from_static("zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3"));
+    headers.insert(ACCEPT_ENCODING, HeaderValue::from_static("gzip, deflate, br"));
+    let response = client
+        .get(booking_page_url)
+        .headers(headers.clone())
+        .send()?;
+
+    // Find session ID
+    let session_id_cookie = response.cookies().find(|cookie| cookie.name() == "JSESSIONID").unwrap();
+    println!("JSESSIONID: {}", session_id_cookie.value());
+
+    let response_text = response.text()?;
+
+    let document = Html::parse_document(&response_text);
+    let selector = Selector::parse("#BookingS1Form_homeCaptcha_passCode").unwrap();
+    let element = document.select(&selector).next().expect("Couldn't find the captcha element");
+    let src = element.value().attr("src").expect("Couldn't find the captcha source url");
+    let captcha_url = ["https://irs.thsrc.com.tw", src].concat();
+    println!("Captcha url: {}", captcha_url);
+    download_captcha(&client, captcha_url.as_str(), headers, captcha_local_path)?;
+    opener::open(captcha_local_path)?;
+
     // // Get user input for CAPTCHA
     // println!("Type the answer to the CAPTCHA: ");
     // let mut captcha_solution = String::new();
@@ -172,35 +178,39 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO Fake the booking parameters for now
     // TODO Use serde to model the booking parameters
 
-    let booking = Booking{
-        start_station: Station::Nangang,
-        dest_station: Station::Zuouing,
-        search_by: String::from("radio31"),
-        types_of_trip: Trip::OneWay,
-        // TODO test
-        // outbound_datetime: DateTime::parse_from_str("2025/01/21 10:00 AM", "%Y/%m/%d %H:%M")
-        outbound_date: String::from("2025/01/21"),
-        outbound_time: String::from("1000A"),
-        security_code: String::from("abcd"),
-        seat_prefer: SeatPref::Window,
-        form_mark: String::from(""),
-        class_type: CabinClass::Business,
-        inbound_date: None,
-        inbound_time: None,
-        to_train_id: None,
-        back_train_id: None,
-        adult_ticket_num: String::from("1F"),
-        child_ticket_num: String::from("0H"),
-        disabled_ticket_num: String::from("0W"),
-        elder_ticket_num: String::from("2E"),
-        college_ticket_num: String::from("0P"),
-    };
-    println!("booking: {:?}", booking);
-    println!("booking (json): {}", serde_json::to_string(&booking).unwrap());
-
     // TODO test
-    // let dt = NaiveDateTime::parse_from_str("2025/01/27 22:00", "%Y/%m/%d %H:%M").unwrap();
-    // println!("datetime: {}", dt);
+    // let booking = Booking{
+    //     start_station: Station::Nangang,
+    //     dest_station: Station::Zuouing,
+    //     search_by: String::from("radio31"),
+    //     types_of_trip: Trip::OneWay,
+    //     // TODO test
+    //     // outbound_datetime: DateTime::parse_from_str("2025/01/21 10:00 AM", "%Y/%m/%d %H:%M")
+    //     outbound_date: String::from("2025/01/21"),
+    //     outbound_time: String::from("1000A"),
+    //     security_code: String::from("abcd"),
+    //     seat_prefer: SeatPref::Window,
+    //     form_mark: String::from(""),
+    //     class_type: CabinClass::Business,
+    //     inbound_date: None,
+    //     inbound_time: None,
+    //     to_train_id: None,
+    //     back_train_id: None,
+    //     adult_ticket_num: String::from("1F"),
+    //     child_ticket_num: String::from("0H"),
+    //     disabled_ticket_num: String::from("0W"),
+    //     elder_ticket_num: String::from("2E"),
+    //     college_ticket_num: String::from("0P"),
+    // };
+    // println!("booking: {:?}", booking);
+    // println!("booking (json): {}", serde_json::to_string(&booking).unwrap());
+    //
+    // // TODO test
+    // // let dt = NaiveDateTime::parse_from_str("2025/01/27 22:00", "%Y/%m/%d %H:%M").unwrap();
+    // // println!("datetime: {}", dt);
+    //
+    // // Submit booking form
+    // let submit_booking_form_url = format!("https://irs.thsrc.com.tw/IMINT/;jsessionid={}?wicket:interface=:0:BookingS1Form::IFormSubmitListener", client.);
 
     Ok(())
 }
